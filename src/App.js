d9b112576f399cac5233f36ab6c5451f0da5588f
MikeNotu <mikebateman98@gmail.com>
2021-11-18 11:44:06 -0600

    Passing Data Via Props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Table from "./Components/Table";
 
-function App() {
+function App(props) {
     const [state, setState] = useState(0);
 
     function name() {
@@ -11,12 +11,14 @@ function App() {
 
     useEffect(() => {}, []);
 
+    const Values = [{ number: 1001 }, { number: 20 }, { number: 30 }];
+
     const numbers = state < 10 ? <p>{state}</p> : null;
     return (
         <div className="App">
             {numbers}
             <button onClick={name}>+1</button>
-            <Table />
+            <Table number={Values[0].number}></Table>
         </div>
     );
 }
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,11 +1,13 @@
-function Table(params) {
+function Table(props) {
+    const Values = [{ number: 10 }, { number: 20 }, { number: 30 }];
+    const TrueValue = 100;
     return (
         <div>
             <table>
                 <tr>
-                    <th>1</th>
-                    <th>2</th>
-                    <th>3</th>
+                    <th>{TrueValue}</th>
+                    <th>{props.number}</th>
+                    <th>{Values[1].number}</th>
                 </tr>
                 <tr>
                     <th>1</th>
