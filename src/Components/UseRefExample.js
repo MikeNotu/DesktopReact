b20c21da93111a5ec886348995e94cdda0f6cf98
MikeNotu <mikebateman98@gmail.com>
2021-12-01 11:50:46 -0600

    useRef 2

--- src/Components/UseRefExample.js
+++ src/Components/UseRefExample.js
@@ -3,15 +3,24 @@ import { useRef } from "react";
 const UseRefExample = () => {
     const inputRef = useRef(null);
 
+    const inputRefTwo = useRef(null);
+
     const onClick = () => {
         inputRef.current.focus();
+    };
+
+    const onClickTwo = () => {
+        inputRefTwo.current.value = "";
     };
 
     return (
         <div>
             <h1>Mike</h1>
             <input type="text" placeholder="Example...." ref={inputRef} />
-            <button onClick={onClick}>Change Name2</button>
+            <button onClick={onClick}>Focus</button>
+
+            <input type="text" placeholder="Example...." ref={inputRefTwo} />
+            <button onClick={onClickTwo}>Clear Text</button>
         </div>
     );
 };
