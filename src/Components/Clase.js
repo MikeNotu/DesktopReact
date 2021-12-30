6a7f38d35224c09319b5888030efa1dcfe1dff33
MikeNotu <mikebateman98@gmail.com>
2021-12-30 15:26:30 -0600

    Class component: Increment via button

--- src/Components/Clase.js
+++ src/Components/Clase.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 class Clase extends Component {
-    // constructor(props) {
-    // console.log(props);
-    // super(props);
-    // this.state = { number: 5 };
-    // }
+    constructor(props) {
+        console.log(props);
+        super(props);
+        this.state = { count: 0 };
+    }
+
+    increment() {
+        this.setState({ count: this.state.count + 1 });
+    }
+
     state = { number: 5 };
     render() {
         const { number, name } = this.props;
@@ -13,6 +18,8 @@ class Clase extends Component {
         return (
             <h1>
                 Class Component {name} {number}
+                <div>Count: {this.state.count}</div>
+                <button onClick={() => this.increment()}>Increment</button>
             </h1>
         );
     }
