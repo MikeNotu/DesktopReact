126afcdabf13b7db6a4408f856d3c44eab8a8130
MikeNotu <mikebateman98@gmail.com>
2022-07-06 19:16:14 -0500

    Explicando React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import "./App.css";
 import React from "react";
-import Button from "./Components/Button";
-import Countdown from "./Components/Countdown";
 
 function App() {
 
-var Rick = [10,20,30];
+    var sueldo = 10;
+    var nombre = "Juan";
+    var prendido = true;
+
+    let sueldo2 = 10;
+    const arizona = 13;
+
+    var cliente = {
+        nombre: "Pedro",
+        edad: 20,
+        casado: false,
+    }
+
+    var arreglo = [1,20,400,10,30]
+
 
     return (
         <div>
-            <h1>Hola Mundo, soy React!</h1>
-            <h2>Hola Mundo, soy React!</h2>
-            <h3>Hola Mundo, soy React!</h3>
-            {Rick[2]}
-            <Button/>
-            <Countdown/>
+         <h1 className="mike">Titulo</h1> 
+         {cliente.edad}
         </div>
     );
 }
